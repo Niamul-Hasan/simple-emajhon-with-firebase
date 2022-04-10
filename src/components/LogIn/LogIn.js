10dc7ec836d1e62d29cc0e0c0ebf8f06929c08b1
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-04-10 21:44:03 +0600

    react-firebase-hook is imported

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import "./LogIn.css";
+
+const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [signInWithEmailAndPassword,
+        user,
+        loading,
+        error,] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const navigate = useNavigate();
+    if (user) {
+        navigate("/shop");
+    }
+
+    const handleLogInUser = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+
+    }
+    return (
+        <div className="form-container">
+            <div>
+                <h2 className='form-title'>Log In</h2>
+                <form onSubmit={handleLogInUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    <input className='form-submit' type="submit" value="Log in" />
+                </form>
+                <p>New in ema john? <Link className='form-link' to='/signup'>Create An Account</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default LogIn;
